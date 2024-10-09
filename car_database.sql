2277e78886e9dde3d2357389b800649c36494179
gunayazizova <gunayazizova@hotmail.com>
2024-10-09 10:00:25 +0200

    updated_car_database

--- car_database.sql
+++ car_database.sql
@@ -21,17 +21,19 @@ USE `car_database` ;
 -- Table `car_database`.`customers`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `car_database`.`customers` (
-  `customer ID` INT NOT NULL AUTO_INCREMENT,
+  `auto_increment_id` INT NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(45) NOT NULL,
   `phone_number` VARCHAR(45) NULL DEFAULT NULL,
+  `customer ID` INT NOT NULL,
   `email` VARCHAR(45) NOT NULL,
   `address` VARCHAR(45) NOT NULL,
   `city` VARCHAR(45) NOT NULL,
   `state/province` VARCHAR(45) NOT NULL,
   `country` VARCHAR(45) NOT NULL,
   `zip/postal code` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`customer ID`),
-  UNIQUE INDEX `idcustomer_UNIQUE` (`customer ID` ASC) VISIBLE)
+  PRIMARY KEY (`auto_increment_id`, `customer ID`),
+  UNIQUE INDEX `idcustomer_UNIQUE` (`auto_increment_id` ASC) VISIBLE,
+  UNIQUE INDEX `auto_increment_id_UNIQUE` (`customer ID` ASC) VISIBLE)
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8mb3;
 
@@ -40,10 +42,12 @@ DEFAULT CHARACTER SET = utf8mb3;
 -- Table `car_database`.`salespersons`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `car_database`.`salespersons` (
-  `staff_id` INT NOT NULL AUTO_INCREMENT,
+  `auto_increment_id` INT NOT NULL AUTO_INCREMENT,
   `store` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`staff_id`),
-  UNIQUE INDEX `staff_id_UNIQUE` (`staff_id` ASC) VISIBLE)
+  `staff_id` INT NOT NULL,
+  PRIMARY KEY (`auto_increment_id`, `staff_id`),
+  UNIQUE INDEX `staff_id_UNIQUE` (`auto_increment_id` ASC) VISIBLE,
+  UNIQUE INDEX `auto_increment_id_UNIQUE` (`staff_id` ASC) VISIBLE)
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8mb3;
 
@@ -52,25 +56,27 @@ DEFAULT CHARACTER SET = utf8mb3;
 -- Table `car_database`.`invoices`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `car_database`.`invoices` (
+  `auto_increment_id` INT NOT NULL AUTO_INCREMENT,
   `date` DATETIME NULL DEFAULT NULL,
   `customer` VARCHAR(45) NOT NULL,
-  `invoice_number` INT NOT NULL AUTO_INCREMENT,
+  `invoice_number` INT NOT NULL,
   `salespersons_auto_increment_id` INT NOT NULL,
   `salespersons_staff_id` INT NOT NULL,
   `customers_auto_increment_id` INT NOT NULL,
   `customers_customer ID` INT NOT NULL,
   `car` VARCHAR(45) NOT NULL,
   `salesperson` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`invoice_number`),
+  PRIMARY KEY (`auto_increment_id`, `invoice_number`, `customers_auto_increment_id`, `customers_customer ID`),
+  UNIQUE INDEX `invoice_number_UNIQUE` (`auto_increment_id` ASC) VISIBLE,
   UNIQUE INDEX `auto_increment_id_UNIQUE` (`invoice_number` ASC) VISIBLE,
   INDEX `fk_invoices_salespersons1_idx` (`salespersons_auto_increment_id` ASC, `salespersons_staff_id` ASC) VISIBLE,
   INDEX `fk_invoices_customers1_idx` (`customers_auto_increment_id` ASC, `customers_customer ID` ASC) VISIBLE,
   CONSTRAINT `fk_invoices_customers1`
-    FOREIGN KEY (`customers_auto_increment_id`)
-    REFERENCES `car_database`.`customers` (`customer ID`),
+    FOREIGN KEY (`customers_auto_increment_id` , `customers_customer ID`)
+    REFERENCES `car_database`.`customers` (`auto_increment_id` , `customer ID`),
   CONSTRAINT `fk_invoices_salespersons1`
-    FOREIGN KEY (`salespersons_auto_increment_id`)
-    REFERENCES `car_database`.`salespersons` (`staff_id`))
+    FOREIGN KEY (`salespersons_auto_increment_id` , `salespersons_staff_id`)
+    REFERENCES `car_database`.`salespersons` (`auto_increment_id` , `staff_id`))
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8mb3;
 
@@ -79,33 +85,28 @@ DEFAULT CHARACTER SET = utf8mb3;
 -- Table `car_database`.`car`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `car_database`.`car` (
+  `auto_increment_id` INT NOT NULL AUTO_INCREMENT,
   `Model` VARCHAR(45) NOT NULL,
   `Colour` VARCHAR(45) NOT NULL,
-  `VIN` INT NOT NULL AUTO_INCREMENT,
-  `customers_customer ID` INT NOT NULL,
-  `customers_auto_increment_id` INT NOT NULL,
+  `VIN` INT NOT NULL,
   `invoices_invoice_number` INT NOT NULL,
   `invoices_auto_increment_id` INT NOT NULL,
   `salespersons_auto_increment_id` INT NOT NULL,
   `salespersons_staff_id` INT NOT NULL,
   `manufacturer` VARCHAR(45) NOT NULL,
   `year` INT NOT NULL,
-  `carcol` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`VIN`),
+  PRIMARY KEY (`auto_increment_id`, `VIN`),
+  UNIQUE INDEX `VIN_UNIQUE` (`auto_increment_id` ASC) VISIBLE,
   UNIQUE INDEX `Model_UNIQUE` (`Model` ASC) VISIBLE,
   UNIQUE INDEX `auto_increment_id_UNIQUE` (`VIN` ASC) VISIBLE,
-  INDEX `fk_car_customers_idx` (`customers_customer ID` ASC, `customers_auto_increment_id` ASC) VISIBLE,
   INDEX `fk_car_invoices1_idx` (`invoices_invoice_number` ASC, `invoices_auto_increment_id` ASC) VISIBLE,
   INDEX `fk_car_salespersons1_idx` (`salespersons_auto_increment_id` ASC, `salespersons_staff_id` ASC) VISIBLE,
-  CONSTRAINT `fk_car_customers`
-    FOREIGN KEY (`customers_customer ID`)
-    REFERENCES `car_database`.`customers` (`customer ID`),
   CONSTRAINT `fk_car_invoices1`
-    FOREIGN KEY (`invoices_auto_increment_id`)
-    REFERENCES `car_database`.`invoices` (`invoice_number`),
+    FOREIGN KEY (`invoices_invoice_number` , `invoices_auto_increment_id`)
+    REFERENCES `car_database`.`invoices` (`auto_increment_id` , `invoice_number`),
   CONSTRAINT `fk_car_salespersons1`
-    FOREIGN KEY (`salespersons_auto_increment_id`)
-    REFERENCES `car_database`.`salespersons` (`staff_id`))
+    FOREIGN KEY (`salespersons_auto_increment_id` , `salespersons_staff_id`)
+    REFERENCES `car_database`.`salespersons` (`auto_increment_id` , `staff_id`))
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8mb3;
 
@@ -118,15 +119,15 @@ CREATE TABLE IF NOT EXISTS `car_database`.`salespersons_has_customers` (
   `salespersons_staff_id` INT NOT NULL,
   `customers_auto_increment_id` INT NOT NULL,
   `customers_customer ID` INT NOT NULL,
-  PRIMARY KEY (`salespersons_auto_increment_id`, `salespersons_staff_id`, `customers_auto_increment_id`, `customers_customer ID`),
+  PRIMARY KEY (`salespersons_auto_increment_id`, `salespersons_staff_id`),
   INDEX `fk_salespersons_has_customers_customers1_idx` (`customers_auto_increment_id` ASC, `customers_customer ID` ASC) VISIBLE,
   INDEX `fk_salespersons_has_customers_salespersons1_idx` (`salespersons_auto_increment_id` ASC, `salespersons_staff_id` ASC) VISIBLE,
   CONSTRAINT `fk_salespersons_has_customers_customers1`
-    FOREIGN KEY (`customers_auto_increment_id`)
-    REFERENCES `car_database`.`customers` (`customer ID`),
+    FOREIGN KEY (`customers_auto_increment_id` , `customers_customer ID`)
+    REFERENCES `car_database`.`customers` (`auto_increment_id` , `customer ID`),
   CONSTRAINT `fk_salespersons_has_customers_salespersons1`
-    FOREIGN KEY (`salespersons_auto_increment_id`)
-    REFERENCES `car_database`.`salespersons` (`staff_id`))
+    FOREIGN KEY (`salespersons_auto_increment_id` , `salespersons_staff_id`)
+    REFERENCES `car_database`.`salespersons` (`auto_increment_id` , `staff_id`))
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8mb3;
 
